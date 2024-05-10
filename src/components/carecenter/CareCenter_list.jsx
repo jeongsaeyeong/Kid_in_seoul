aba95a9473d34f9db917beb7b5f9042956c09e0c
Backjangme <144644177+Backjangme@users.noreply.github.com>
2024-05-10 22:09:59 +0900

    m

--- src/components/carecenter/CareCenter_list.jsx
+++ src/components/carecenter/CareCenter_list.jsx
@@ -7,7 +7,7 @@ import axios from 'axios'
 import { useSelector } from 'react-redux'
 
 const CareCenter_list = ({ loading, setSearch, search, setAddress, type, setSelect, setAdd }) => {
-    const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+    const PROXY = process.env.REACT_APP_SERVER_URL;
     
     const user = useSelector((state => state.user))
     const params = useParams();
